4a9509666469f959715b1fcb1ebf7f623848d91d
Dabasan <43123127+Dabasan@users.noreply.github.com>
2019-12-28 12:02:51 +0900

    Updated FileFunctions to read and write through a stream.

--- src/com/daxie/tool/FileFunctions.java
+++ src/com/daxie/tool/FileFunctions.java
@@ -11,7 +11,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -27,21 +29,18 @@ import com.daxie.log.LogFile;
 public class FileFunctions {
 	/**
 	 * Returns all lines of a text file.
-	 * @param filename Filename to load
+	 * @param fis Stream
 	 * @param encoding Encoding of the file
 	 * @return A list of strings that are split per line
-	 * @throws FileNotFoundException Specified file not found
 	 * @throws UnsupportedEncodingException Specified encoding not supported
 	 */
-	public static List<String> GetFileAllLines(String filename,String encoding) throws FileNotFoundException,UnsupportedEncodingException{
+	public static List<String> GetFileAllLines(InputStream fis,String encoding) throws UnsupportedEncodingException{
 		List<String> ret=new ArrayList<>();
 		
 		StringBuilder sb=new StringBuilder("");
 		BufferedReader br=null;
 		
-		br=new BufferedReader(
-				new InputStreamReader(
-						new FileInputStream(filename),encoding));
+		br=new BufferedReader(new InputStreamReader(fis,encoding));
 		
 		try {	
 			while(true) {
@@ -84,18 +83,30 @@ public class FileFunctions {
 		return ret;
 	}
 	/**
-	 * Returns all bytes of a binary file.
+	 * Returns all lines of a text file.
 	 * @param filename Filename to load
-	 * @return A list of bytes
+	 * @param encoding Encoding of the file
+	 * @return A list of strings that are split per line
 	 * @throws FileNotFoundException Specified file not found
+	 * @throws UnsupportedEncodingException Specified encoding not supported
 	 */
-	public static List<Byte> GetFileAllBin(String filename) throws FileNotFoundException{
+	public static List<String> GetFileAllLines(String filename,String encoding) 
+			throws FileNotFoundException,UnsupportedEncodingException{
+		FileInputStream fis=new FileInputStream(filename);
+		List<String> ret=GetFileAllLines(fis, encoding);
+		
+		return ret;
+	}
+	/**
+	 * Returns all bytes of a binary file.
+	 * @param fis Stream
+	 * @return A list of bytes
+	 */
+	public static List<Byte> GetFileAllBin(InputStream fis){
 		List<Byte> bin=new ArrayList<Byte>();
 		
 		DataInputStream dis;	
-		dis=new DataInputStream(
-				new BufferedInputStream(
-						new FileInputStream(filename)));
+		dis=new DataInputStream(new BufferedInputStream(fis));
 		
 		try {
 			byte read_byte;
@@ -132,19 +143,28 @@ public class FileFunctions {
 		return bin;
 	}
 	/**
+	 * Returns all bytes of a binary file.
+	 * @param filename Filename to load
+	 * @return A list of bytes
+	 * @throws FileNotFoundException Specified file not found
+	 */
+	public static List<Byte> GetFileAllBin(String filename) throws FileNotFoundException{
+		FileInputStream fis=new FileInputStream(filename);
+		List<Byte> bin=GetFileAllBin(fis);
+		
+		return bin;
+	}
+	/**
 	 * Creates a text file.
-	 * @param filename Filename
+	 * @param fos Stream
 	 * @param encoding Encoding
 	 * @param lines Lines to write in the file
-	 * @exception FileNotFoundException Thrown when it fails to write
 	 * @exception UnsupportedEncodingException Unknown encoding specified
 	 */
-	public static void CreateTextFile(String filename,String encoding,List<String> lines) 
-			throws FileNotFoundException,UnsupportedEncodingException{
+	public static void CreateTextFile(OutputStream fos,String encoding,List<String> lines) 
+			throws UnsupportedEncodingException{
 		BufferedWriter br=null;
-		br=new BufferedWriter(
-				new OutputStreamWriter(
-						new FileOutputStream(filename),encoding));
+		br=new BufferedWriter(new OutputStreamWriter(fos,encoding));
 		
 		try {
 			for(String line:lines) {
@@ -175,16 +195,26 @@ public class FileFunctions {
 		}
 	}
 	/**
-	 * Creates a binary file.
+	 * Creates a text file.
 	 * @param filename Filename
-	 * @param bin A list of bytes
+	 * @param encoding Encoding
+	 * @param lines Lines to write in the file
 	 * @exception FileNotFoundException Thrown when it fails to write
+	 * @exception UnsupportedEncodingException Unknown encoding specified
 	 */
-	public static void CreateBinFile(String filename,List<Byte> bin) throws FileNotFoundException{
+	public static void CreateTextFile(String filename,String encoding,List<String> lines) 
+			throws FileNotFoundException,UnsupportedEncodingException{
+		FileOutputStream fos=new FileOutputStream(filename);
+		CreateTextFile(fos, encoding, lines);
+	}
+	/**
+	 * Creates a binary file.
+	 * @param fos Stream
+	 * @param bin A list of bytes
+	 */
+	public static void CreateBinFile(OutputStream fos,List<Byte> bin) {
 		DataOutputStream dos=null;
-		dos=new DataOutputStream(
-				new BufferedOutputStream(
-						new FileOutputStream(filename)));
+		dos=new DataOutputStream(new BufferedOutputStream(fos));
 		
 		try {
 			for(Byte b:bin) {
@@ -210,5 +240,15 @@ public class FileFunctions {
 				return;
 			}
 		}
+	}
+	/**
+	 * Creates a binary file.
+	 * @param filename Filename
+	 * @param bin A list of bytes
+	 * @exception FileNotFoundException Thrown when it fails to write
+	 */
+	public static void CreateBinFile(String filename,List<Byte> bin) throws FileNotFoundException{
+		FileOutputStream fos=new FileOutputStream(filename);
+		CreateBinFile(fos, bin);
 	}
 }
